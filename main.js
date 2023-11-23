31ef0ca6d9d3c571525dc3abae2a06a2f8e6d23d
1ureka <summerheadquarters001@gmail.com>
2023-11-23 17:21:20 +0800

    初步製作content box

--- main.js
+++ main.js
@@ -19,4 +19,11 @@ $(document).ready(function () {
 
   // 創建側邊攔 - 資料夾選單 (之後加上自訂事件偵測，比如 onMain 之類的)
   const select = createFolderSelect().appendTo("#sidebar");
+
+  const folderbox1 = new FolderBox({ bulbColor: "#8ce197", label: "自然" });
+  const folderbox2 = new FolderBox({ bulbColor: "#ffff7a", label: "物件" });
+  const folderbox3 = new FolderBox({ bulbColor: "#92e9ff", label: "場景" });
+  folderbox1.appendTo("#content");
+  folderbox2.appendTo("#content");
+  folderbox3.appendTo("#content");
 });

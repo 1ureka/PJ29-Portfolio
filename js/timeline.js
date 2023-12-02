9bab3d205152d14dbe739e15b464b747aa70dcf5
1ureka <summerheadquarters001@gmail.com>
2023-12-02 22:18:55 +0800

    配合上一個提交，新增時間軸。將一些時間軸改為general

--- js/timeline.js
+++ js/timeline.js
@@ -156,28 +156,24 @@ function createFullscreenIconHoverTl(container) {
 }
 
 function createSortButtonHoverTl(button) {
-  const buttonTimeline = gsap
+  gsap.set(button.find(".sort-button-layer2 > *"), { y: -40 });
+
+  const tl = gsap
     .timeline({ defaults: { duration: 0.2, ease: "set1" }, paused: true })
     .to(button, { x: 10, backgroundColor: "#ea81af" })
-    .fromTo(button, { scale: 1 }, { scale: 1.05 }, "<");
+    .fromTo(button, { scale: 1 }, { scale: 1.05 }, "<")
+    .to(
+      button.find(".sort-button-layer1 > *"),
+      { y: 40, stagger: 0.2 / 3 },
+      "<"
+    )
+    .to(
+      button.find(".sort-button-layer2 > *"),
+      { y: 0, stagger: 0.2 / 3 },
+      "<"
+    );
 
-  return buttonTimeline;
-}
-
-function createFolderSelectOpenTl(select) {
-  const main = select.find(".folder-button").eq(0);
-  const pages = select.find(".folder-button").not(main);
-  const hr = select.find(".h-separator");
-  const timeline = gsap
-    .timeline({
-      defaults: { duration: 0.2, ease: "set1" },
-      paused: true,
-    })
-    .from(hr, { width: 0 })
-    .from(pages, { ease: "back.out(2)", scale: 0.1, stagger: 0.1 }, "<")
-    .from(pages, { autoAlpha: 0, stagger: 0.1 }, "<");
-
-  return timeline;
+  return tl;
 }
 
 function createOutlineTl(element, config) {
@@ -374,4 +370,65 @@ function createColorHoverTl(element, toColor, duration = 0.2) {
     .to(element, { backgroundColor: toColor });
 
   return tl;
+}
+
+function createTranslateHoverTl(element, toX, toY) {
+  const tl = gsap
+    .timeline({
+      defaults: { duration: 0.2, ease: "set1" },
+      paused: true,
+    })
+    .to(element, { x: toX, y: toY });
+
+  return tl;
+}
+
+function createZIndexHoverTl(element, fromZ, toZ) {
+  const tl = gsap
+    .timeline({
+      defaults: { duration: 0.2, ease: "set1" },
+      paused: true,
+    })
+    .fromTo(element, { zIndex: fromZ }, { zIndex: toZ });
+
+  return tl;
+}
+
+function createToggleTl(toggler) {
+  const inner = toggler.find(".toggler-inner");
+  const dot = toggler.find(".toggler-dot");
+
+  const tl = gsap
+    .timeline({ defaults: { duration: 0.2, ease: "set1" }, paused: true })
+    .to(dot, { x: 25, backgroundColor: "hsl(210, 0%, 25%)" })
+    .to(inner, { backgroundColor: "#ea81af" }, "<");
+
+  return tl;
+}
+
+function createTogglerHoverTl(toggler) {
+  const dot = toggler.find(".toggler-dot");
+
+  const tl = gsap
+    .timeline({ defaults: { duration: 0.2, ease: "set1" }, paused: true })
+    .fromTo(toggler, { filter: "brightness(1)" }, { filter: "brightness(1.5)" })
+    .to(dot, { scale: 1.1 }, "<");
+
+  return tl;
+}
+
+function createSelectOpenTl(select) {
+  const main = select.find("button").eq(0);
+  const pages = select.find("button").not(main);
+  const hr = select.find(".h-separator");
+  const timeline = gsap
+    .timeline({
+      defaults: { duration: 0.2, ease: "set1" },
+      paused: true,
+    })
+    .from(hr, { width: 0 })
+    .from(pages, { ease: "back.out(2)", scale: 0.1, stagger: 0.1 }, "<")
+    .from(pages, { autoAlpha: 0, stagger: 0.1 }, "<");
+
+  return timeline;
 }
